caa14a0b9d7048ec48a4b16383db95f68804babb
AbimbolaP <abimbola.peace131@gmail.com>
2024-04-04 15:15:44 +0100

    Initial commit

---
+++ app/api/register/route.ts
@@ -0,0 +1,35 @@
+import bcrypt from "bcrypt";
+import prisma from "@/app/libs/prismadb";
+import { NextResponse } from "next/server";
+export async function POST(
+  request: Request
+)
+  // { try 
+   {
+  const body = await request.json();
+  const {
+    email,
+    name,
+    password
+  } = body;
+
+  const hashedPassword = await bcrypt.hash(password, 12);
+
+  const user = await prisma.user.create({
+    data: {
+      email,
+      name,
+      hashedPassword,
+    }
+  });
+
+  return NextResponse.json(user);
+// } catch (error) {
+//   // Handle errors and return an appropriate response
+//   console.error('Error creating user:', error);
+//   return NextResponse.json({ error: 'Failed to create user' }, { status: 500 });
+// } finally {
+//   // Disconnect the Prisma client
+//   await prisma.$disconnect();
+// }
+}
---
+++ app/layout.tsx
@@ -0,0 +1,46 @@
+import type { Metadata } from "next";
+import { Nunito } from "next/font/google";
+import "./globals.css";
+import Navbar from "./components/navbar/Navbar";
+
+import RegisterModal from "./components/modals/RegisterModal";
+import LoginModal from "./components/modals/LoginModal";
+import RentModal from "./components/modals/RentModal";
+
+import ToasterProvider from "./providers/ToasterProvider";
+import getCurrentUser from "./actions/getCurrentUser";
+import SearchModal from "./components/modals/SearchModal";
+
+// import ClientOnly from "./components/ClientOnly";
+
+const font = Nunito({subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "AirBeeAndBee",
+  description: "Airbnb clone",
+};
+
+export default  async function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const currentUser = await getCurrentUser();
+  return (
+    <html lang="en">
+      <body className={font.className}>
+         {/* <ClientOnly>  */}
+          <ToasterProvider />
+          <SearchModal />
+          <RentModal />
+          <RegisterModal />
+          <LoginModal />
+          <Navbar currentUser={currentUser} />
+        {/* </ClientOnly> */}
+        <div className="pb-20 pt-28">
+        {children}
+        </div>
+      </body>
+    </html>
+  );
+}
